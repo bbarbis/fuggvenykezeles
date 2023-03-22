a018d80baba4023d0d404c6f3f75eb23d6a43f37
bbarbis <baratebarbara2020@gmail.com>
2023-03-22 11:16:18 +0100

    veletlen, sorrend, rendezes metodusok fuggvenyben

--- adat.js
+++ adat.js
@@ -1,2 +1,9 @@
 export const SZAMLISTA= [23, 5, 55, 11, 121];
-export const SZOVEGLISTA =["béka", "alma", "zeller", "eper"];
+export const SZOVEGLISTA =["paradicsom", "alma", "zeller", "eper"];
+export const OBJEKTUMLISTA = [
+    {nev: "Dézi", fajta: "keverék", kor: 2},
+    {nev: "Bodri", fajta: "mudi", kor: 12},
+    {nev: "Loki", fajta: "tacskó", kor: 5},
+    {nev: "Zuri", fajta: "foxterrier", kor: 4},
+    {nev: "Happy", fajta: "némethjuhász", kor: 42},
+];
--- script.js
+++ script.js
@@ -1,17 +1,87 @@
-import {SZAMLISTA, SZOVEGLISTA} from "./adat.js";
+import {OBJEKTUMLISTA, SZAMLISTA, SZOVEGLISTA} from "./adat.js";
 
 
-$(function(){
+$(() => {
     console.log(SZOVEGLISTA);
-    rendezesSzoveg(SZOVEGLISTA);/**lista cimet, referencia kerul atadasra */
-    console.log(SZOVEGLISTA);
-    console.log("********************");
-    console.log(SZOVEGLISTA);
-    rendezesSzoveg(SZOVEGLISTA);/**lista cimet, referencia kerul atadasra */
+    rendezesSzoveg(SZOVEGLISTA); /**lista cimet, referencia kerul atadasra */
     console.log(SZOVEGLISTA);
     console.log("********************");
+
+    console.log(SZAMLISTA);
+    rendezesSzam(SZAMLISTA); /**lista cimet, referencia kerul atadasra */
+    console.log(SZAMLISTA);
+    console.log("********************");
+
+    console.log(SZAMLISTA);
+    veletlenSorrend(SZAMLISTA); /**lista cimet, referencia kerul atadasra */
+    console.log(SZAMLISTA);
+    console.log("********************");
+
+    console.log(SZOVEGLISTA);
+    veletlenSorrend(SZOVEGLISTA); /**lista cimet, referencia kerul atadasra */
+    console.log(SZOVEGLISTA);
+    console.log("********************");
+
+
+    rendezesSzamSzerint(OBJEKTUMLISTA, "kor"); /**lista cimet, referencia kerul atadasra */
+    console.log(OBJEKTUMLISTA);
+    console.log("********************");
+
+
+    rendezesSzovegSzerint(OBJEKTUMLISTA, "fajta"); /**lista cimet, referencia kerul atadasra */
+    console.log(OBJEKTUMLISTA);
+    console.log("********************");
+
+
+    rendezesSzovegSzerint(OBJEKTUMLISTA, "nev"); /**lista cimet, referencia kerul atadasra */
+    console.log(OBJEKTUMLISTA);
+    console.log("********************");
+
 })
 function rendezesSzoveg(lista){
     /**rendezzuk abc sorrendbe */
     lista.sort();
+}
+function rendezesSzam(lista){
+    /**rendezzuk abc sorrendbe */
+    lista.sort(function(a,b){
+        console.log(a,b,a-b)
+        return a-b //ez az ertek negativ, megcsereli a ket elemet, ha pozitiv nem cserel
+    });
+}
+function veletlenSorrend(lista){
+    /**rendezzuk abc sorrendbe */
+    lista.sort(function(a,b){
+        return Math.random()-0.5; //ez az ertek negativ, megcsereli a ket elemet, ha pozitiv nem cserel
+    });
+}
+
+function rendezesSzamSzerint(lista, kulcs){
+    /**rendezzuk abc sorrendbe */
+    lista.sort(function(a, b){
+        console.log(a,b)
+        return a[kulcs]-b[kulcs]//ez az ertek negativ, megcsereli a ket elemet, ha pozitiv nem cserel
+    });
+}
+/**irjuk át ezt a metodust hogy tetszoleges szoveges kulcsra mükődjön */
+function rendezesSzovegSzerint(lista, kulcs){
+    /**rendezzuk abc sorrendbe */
+    lista.sort(function(a, b){
+        if (a[kulcs] > b[kulcs]){
+            return 1;
+        } else{
+            return -1;
+        }
+       //ez az ertek negativ, megcsereli a ket elemet, ha pozitiv nem cserel
+
+    });
+}
+
+function rendezesObjektum(lista, kulcs){
+if(typeof kulcs == "number"){
+    rendezesSzamSzerint(lista, kulcs)
+}else{
+    rendezesSzovegSzerint(lista, kulcs)
+
+}
 }
